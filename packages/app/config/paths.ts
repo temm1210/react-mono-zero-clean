43fd379aec56462f43489b00ffa9ab01032b6d29
SongTaecWoo <temm1210@naver.com>
2021-05-10 23:23:41 +0900

    paths파일에 사용할 경로값 정의

---
+++ packages/app/config/paths.ts
@@ -0,0 +1,22 @@
+import path from "path";
+
+// 해당 프로젝트의 전체 디렉터리경로를 반환
+// ex /user/home/{project-name}
+// 어디서 실행되든 항상 같은 값 반환
+const appDirectory = process.cwd();
+
+// 주어진경로(appPath)의 전체 경로를 반환하는 함수
+const resolveAppPath = (appPath: string) => path.resolve(appDirectory, appPath);
+
+// appDirectory경로부터 resolveAppPath의 값으로 넘긴 경로까지의 전체 경로를 구함
+export default {
+  appPath: resolveAppPath("."),
+  buildPath: resolveAppPath("build"),
+  appTsConfig: resolveAppPath("tsconfig.json"),
+  appJsConfig: resolveAppPath("jsconfig.json"),
+  appNodeModules: resolveAppPath("node_modules"),
+  appSrc: resolveAppPath("src"),
+  entryPath: resolveAppPath("src/index.tsx"),
+  publicPath: resolveAppPath("public"),
+  packageJson: resolveAppPath("package.json"),
+};
