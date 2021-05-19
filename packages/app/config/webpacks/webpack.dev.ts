00c280ba0b03506ede47b24d4ce5cec65ffe4ab9
SongTaecWoo <temm1210@naver.com>
2021-05-20 03:06:33 +0900

    webpack.dev.ts에 주석추가

--- packages/app/config/webpacks/webpack.dev.ts
+++ packages/app/config/webpacks/webpack.dev.ts
@@ -30,8 +30,19 @@ const webpackDevelopmentConfig: Configuration = {
 
 // webpack.common.ts파일과 webpackDevelopmentConfig 머지
 const config = mergeWithCustomize({
+  /**
+   * 기본 webpack 옵션과 target webpack옵션을 merge할때 같은 키 값에대한 merge 전략을 구성
+   * 기본정책은 append이며 가끔 인식이 안되는 키값이 있는데(module.rules[0].oneOf), 이럴경우 key값을 추출해서 어떠한 전략을 사용할지 작성해야함
+   * (덮어씌울지, 기존옵션값 뒤에 추가할지 등)
+   * @see {@link https://github.com/survivejs/webpack-merge#mergewithcustomize-customizearray-customizeobject-configuration--configuration} - merge 전략옵션
+   * @param commonOptions - 기본 commonWebpack의 설정값
+   * @param devOptions - mergeWithCustomize의 2번째 인자값의 속성값
+   * @param key - webpack의 property값(entry, module, module.rules,output 등등)
+   */
   customizeArray(commonOptions, devOptions, key) {
     switch (key) {
+      // module.rules에 대해선 oneOf키값에 prepend전략을 사용
+      // baseWebpack의 설정값 앞에 target webpack 설정값을 더함
       case "module.rules":
         return [{ oneOf: [...devOptions[0].oneOf, ...commonOptions[0].oneOf] }];
       default:
