10175d6b0802b26871e28adfeb1947b75e973a1a
SongTaecWoo <temm1210@naver.com>
2021-05-24 00:09:38 +0900

    autoprefix 설정 테스트

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import test2 from "test2";
+import "./index.css";
 
 function App() {
   test2();
-  return <div>App</div>;
+  return <div className="test-css">App</div>;
 }
 
 export default App;
