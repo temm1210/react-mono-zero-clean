38a78a2ef855f5b3b1b2da9065c6009cbe590b8f
SongTaecWoo <temm1210@naver.com>
2021-05-24 04:56:07 +0900

    env, asset d.ts파일 추가

---
+++ packages/app/src/types/envd.d.ts
@@ -0,0 +1,16 @@
+// global타입을 확장시키는 작업
+// .env에 정의한 변수를 여기서도 선언해줘야 타입추론이 이루어짐
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production" | "test";
+      PUBLIC_URL: string;
+      PORT: string;
+      test: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
