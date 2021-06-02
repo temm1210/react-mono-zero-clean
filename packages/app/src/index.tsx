85aa85de598fb2b87ce368402e21f528c793987b
SongTaecWoo <temm1210@naver.com>
2021-06-02 18:40:25 +0900

    image 파일 테스트

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import test2 from "test2";
 import "./index.css";
+import f from "./test.jpeg";
+
+console.log(`test2:${process.env.NODE_ENV}`);
+console.log(`test:${process.env.PUBLIC_URL}`);
+console.log(`test1:${process.env.test}`);
 
 function App() {
   test2();
-  return <div className="test-css">App</div>;
+  return (
+    <div className="test-css">
+      App
+      <img src={f} alt="test" />
+    </div>
+  );
 }
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
 
 export default App;
