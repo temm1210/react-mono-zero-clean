2875447f84f1949fede78d179ea137092cf90fc3
SongTaecWoo <temm1210@naver.com>
2021-06-21 21:12:12 +0900

    주석추가

--- packages/app/config/utils/checkBrowser.ts
+++ packages/app/config/utils/checkBrowser.ts
@@ -3,6 +3,7 @@ import chalk from "chalk";
 
 // browserslist가 정의되어 있는지 확인하는 함수
 function checkBrowsers(dir: string) {
+  // browserslist 의 설정파일을 불러옴
   const current = browserslist.loadConfig({ path: dir });
 
   if (!current) {
