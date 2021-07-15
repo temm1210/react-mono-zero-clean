79da2c272bcedf8e375215d83d2397b51d33735b
SongTaecWoo <temm1210@naver.com>
2021-07-16 00:38:34 +0900

    test2

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -6,11 +6,11 @@ function App() {
   test2();
   // const b: any = "asdfasf";
   // const a: any = "test";
-  // console.log("c:", a);
+  // console.log("c:", b);
   // console.log("c:", b);
   return (
     <div className="test-css">
-      App22
+      test
       <img src={f} alt="test" />
     </div>
   );
