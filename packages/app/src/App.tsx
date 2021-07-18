9bd7b032242c67e5aa7e020dd61609b1d0ff745d
SongTaecWoo <temm1210@naver.com>
2021-07-19 02:14:20 +0900

    router(v6) 설정

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,18 +1,16 @@
-import test2 from "test2";
+import Home from "pages/Home";
+import Test from "pages/Test";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
-import f from "./test.jpeg";
 
 function App() {
-  test2();
-  // const b: any = "asdfasf";
-  // const a: any = "test";
-  // console.log("c:", a);
-  // console.log("c:", b);
   return (
-    <div className="test-css">
-      App22
-      <img src={f} alt="test" />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
   );
 }
 
