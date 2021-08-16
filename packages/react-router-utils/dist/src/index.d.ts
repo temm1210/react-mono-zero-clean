832a3c55dcd416c522b4095595b05e6f97c60e23
SongTaecWoo <temm1210@naver.com>
2021-08-17 03:43:51 +0900

    delete

--- packages/react-router-utils/dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { IRouter, IMapRouterReturn } from "./types";
-declare function mapRouter<R extends IRouter>(routerConfig: R): IMapRouterReturn<R>;
-export default mapRouter;
-//# sourceMappingURL=index.d.ts.map
