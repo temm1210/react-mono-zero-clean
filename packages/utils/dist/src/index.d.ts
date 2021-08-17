683f71a603f415aac162214713d0008b11edbcc6
SongTaecWoo <temm1210@naver.com>
2021-08-18 02:28:29 +0900

    delete

--- packages/utils/dist/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as fp } from "./fp";
-export { default as removeString } from "./removeString";
-//# sourceMappingURL=index.d.ts.map
--- packages/utils/dist/src/removeString/types.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type TStringRemove = "start" | "base" | "end";
-export declare type TStringRemoveStrategy = {
-    [key in TStringRemove]: (target: string, findIndex: number) => string;
-};
-//# sourceMappingURL=types.d.ts.map
