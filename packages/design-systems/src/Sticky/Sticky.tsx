5035ea8eeb5846c2c41b90afebedc7ec0b903317
SongTaecWoo <temm1210@naver.com>
2021-10-01 23:05:37 +0900

    sticky및 type 생성

--- packages/design-systems/src/Sticky/Sticky.tsx
+++ packages/design-systems/src/Sticky/Sticky.tsx
@@ -1,5 +1,10 @@
-function Sticky() {
-  return <div>Sticky</div>;
+export interface Props {
+  /** test */
+  children: React.ReactNode;
+}
+
+function Sticky({ children }: Props) {
+  return <div>{children}</div>;
 }
 
 export default Sticky;
--- packages/design-systems/src/Sticky/index.ts
+++ packages/design-systems/src/Sticky/index.ts
@@ -1,2 +1,4 @@
 export { default as Sticky } from "./Sticky";
+export type { Props as StickyProps } from "./Sticky";
 export { default as StickyContainer } from "./StickyContainer";
+export type { Props as StickyContainerProps } from "./StickyContainer";
