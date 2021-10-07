c0254a3e72758549bc5216973782e5c232ae9871
SongTaecWoo <temm1210@naver.com>
2021-10-08 03:29:28 +0900

    context, useRef 삭제

--- packages/design-systems/src/Sticky/StickyContainer.tsx
+++ packages/design-systems/src/Sticky/StickyContainer.tsx
@@ -1,20 +1,11 @@
-import { createContext, useRef, RefObject } from "react";
 import "./StickyContainer.scss";
 
-export const StickyContext = createContext<RefObject<HTMLDivElement> | null>(null);
 export interface Props {
   children: React.ReactNode;
 }
 
 function StickyContainer({ children }: Props) {
-  const ref = useRef<HTMLDivElement>(null);
-  return (
-    <StickyContext.Provider value={ref}>
-      <div ref={ref} className="sticky-container">
-        {children}
-      </div>
-    </StickyContext.Provider>
-  );
+  return <div className="sticky-container">{children}</div>;
 }
 
 export default StickyContainer;
