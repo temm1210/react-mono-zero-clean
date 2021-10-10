1906ac85c10c4a45e72e01591a991362d1bbc6ec
SongTaecWoo <temm1210@naver.com>
2021-10-10 22:13:36 +0900

    logic 분리

--- packages/design-systems/src/Sticky/Sticky.tsx
+++ packages/design-systems/src/Sticky/Sticky.tsx
@@ -12,40 +12,60 @@ function Sticky({ children, offset = 0 }: Props) {
   const containerRef = useRef<Element>();
 
   // const [isSticky, setIsSticky] = useState(false);
-  // sticky상태에서 context의 bottom에 도달했는지 여부를 체크하는 값
-  // const [isReachBottom, setIsReachBottom] = useState(false);
   const stickyRef = useRef<HTMLDivElement>(null);
   const heightRef = useRef<HTMLDivElement>(null);
 
-  const onUpdate = useCallback(() => {
+  const assignRects = () => {
     const containerRect = containerRef.current?.getBoundingClientRect();
     const heightRect = heightRef.current?.getBoundingClientRect();
     const stickyRect = stickyRef.current?.getBoundingClientRect();
 
-    if (!containerRect || !heightRect || !stickyRect) return;
+    if (!containerRect || !heightRect || !stickyRect) return null;
+    return { containerRect, heightRect, stickyRect };
+  };
 
-    // sticky영역이 위로 올라가는 시점
-    // viewport에서 container의 위치가 sticky element의 높이보다 작아질때
-    const isReachContainerBottom = () => {
-      return stickyRect?.height + offset >= containerRect?.bottom;
-    };
+  /**
+   * calculatePositionHandlers method에서 return하는 타입
+   * @typedef {Object} StickyHandlers
+   * @property {() => boolean} isReachContainerBottom - sticky영역이 위로 올라가는 시점(viewport에서 container의 위치가 sticky element의 높이보다 작아질때)
+   * @property {() => boolean} isReachScreenTop - sticky영역이 현재 viewport상단에 고정되는 시점(offset이 주어지면 그만큼 떨어진상태로 고정)
+   */
+  /**
+   * sticky element의 위치를 확인하여 상태를 계산해주는 methods 모음
+   * @return {StickyHandlers} 현재 상태를 계산해주는 methods
+   */
+  const calculatePositionHandlers = useCallback(() => {
+    const rects = assignRects();
+    if (!rects) return;
 
-    // sticky영역이 현재 viewport상단에 고정되는 시점(offset이 주어지면 그만큼 떨어진상태로 고정)
-    const isReachScreenTop = () => {
-      return heightRect.top < offset;
-    };
+    const { stickyRect, containerRect, heightRect } = rects;
+    const isReachContainerBottom = () => stickyRect?.height + offset >= containerRect?.bottom;
+    const isReachScreenTop = () => heightRect.top < offset;
 
-    console.log("isReachContainerBottom():", isReachContainerBottom());
-    console.log("isReachScreenTop():", isReachScreenTop());
+    return { isReachContainerBottom, isReachScreenTop };
   }, [offset]);
+
+  /**
+   * 현재 sticky element의 상태값을 사용하여 처리하는 함수
+   */
+  const update = useCallback(() => {
+    const handlers = calculatePositionHandlers();
+
+    console.log(handlers);
+  }, [calculatePositionHandlers]);
+
+  const handleUpdate = useCallback(() => {
+    // raf처리
+    update();
+  }, [update]);
+
+  useEvent("scroll", handleUpdate, { passive: true });
+  useEvent("resize", handleUpdate);
 
   const assignContainerClientRect = useCallback((node: HTMLDivElement) => {
     const stickyContainer = node.parentElement?.closest(".sticky-container") || document.body;
     containerRef.current = stickyContainer;
   }, []);
-
-  useEvent("scroll", onUpdate, { passive: true });
-  useEvent("resize", onUpdate);
 
   return (
     <div ref={assignContainerClientRect}>
