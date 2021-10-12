a266121725e06c36a0342b72b1b6bf1d40cc2090
SongTaecWoo <temm1210@naver.com>
2021-10-12 16:05:32 +0900

    sticky handler 작성

--- packages/design-systems/src/Sticky/Sticky.tsx
+++ packages/design-systems/src/Sticky/Sticky.tsx
@@ -2,47 +2,55 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import cx from "clsx";
 import { useEvent, useClosetParent } from "@project/react-hooks";
 import useCalculatePositions from "./hooks/useCalculatePositions";
+import { SinglePosition, StickyHandler } from "./types";
+import PARENT_SELECTOR from "./parentSelector";
 
 export interface Props {
   /** test */
   children: React.ReactNode;
   /** 상단에서 얼마나 떨어진 상태로 sticky가 진행될지 결정 */
   offset?: number;
-  /** Sticky컴포넌트가 mount 됐을때 실행할 callback 함수 */
-  onMount?: () => void;
   /** Sticky컴포넌트가 sticky 됐을때 실행할 callback 함수 */
-  onSticky?: () => void;
+  onStick?: () => void;
   /** Sticky컴포넌트가 unSticky 됐을때 실행할 callback 함수 */
-  onUnSticky?: () => void;
+  onUnStick?: () => void;
 }
 
-type SinglePosition = number | null;
-interface Position {
-  top: SinglePosition;
-  bottom: SinglePosition;
-}
-
-interface StickyStatus {
-  isSticky: boolean;
-  isAbsolute: boolean;
-}
-
-function Sticky({ children, offset = 0, onMount, onSticky, onUnSticky }: Props) {
+function Sticky({ children, offset = 0, onStick, onUnStick }: Props) {
   const [isSticky, setIsSticky] = useState(false);
   const [isAbsolute, setIsIsAbsolute] = useState(false);
   const [top, setTop] = useState<SinglePosition>(0);
   const [bottom, setBottom] = useState<SinglePosition>(0);
 
-  const { parentRef, findParentFrom } = useClosetParent(".sticky-container");
+  const { parentRef, findParentFrom } = useClosetParent(PARENT_SELECTOR);
   const stickyRef = useRef<HTMLDivElement>(null);
   const heightRef = useRef<HTMLDivElement>(null);
 
+  const stickyHandlerRef = useRef<StickyHandler>();
   const calculatePositionHandlers = useCalculatePositions({ containerRef: parentRef, stickyRef, heightRef, offset });
 
-  const fireStickyCallback = (pIsSticky: boolean) => {
-    if (pIsSticky && onSticky) return onSticky();
-    if (!pIsSticky && onUnSticky) return onUnSticky();
-  };
+  /**
+   * 현재 sticky element의 상태값을 사용하여 처리하는 함수
+   */
+  const update = useCallback(() => {
+    const positionHandlers = calculatePositionHandlers();
+    if (!positionHandlers || !stickyHandlerRef) return;
+    const stickyHandler = stickyHandlerRef.current;
+
+    const { isReachContainerBottom, isReachScreenTop } = positionHandlers;
+
+    // sticky 상태일때
+    if (isReachScreenTop()) {
+      if (isReachContainerBottom()) return stickyHandler?.stickToContainerBottom();
+      return stickyHandler?.stickToScreenTop();
+    }
+
+    return stickyHandler?.unStick();
+  }, [calculatePositionHandlers]);
+
+  const handleUpdate = useCallback(() => {
+    update();
+  }, [update]);
 
   /**
    * sticky상태는 다음과 같이 2가지로 다시 나뉨
@@ -51,62 +59,46 @@ function Sticky({ children, offset = 0, onMount, onSticky, onUnSticky }: Props) 
    * 1번과 같은 경우는 position: absolute로 바뀌어야함
    * 2번과 같은 경우는 position: fixed로 바뀌어야함
    */
-  const setSticky = ({ isSticky: pIsSticky, isAbsolute: pIsAbsolute }: StickyStatus) => {
+  const setStickyAndAbsolute = (pIsSticky: boolean, pIsAbsolute: boolean) => {
     setIsSticky(pIsSticky);
     setIsIsAbsolute(pIsAbsolute);
-    fireStickyCallback(pIsSticky);
   };
 
-  const setTopAndBottom = ({ top: pTop, bottom: pBottom }: Position) => {
+  const setTopAndBottom = (pTop: SinglePosition, pBottom: SinglePosition) => {
     setTop(pTop);
     setBottom(pBottom);
   };
 
-  // sticky영역이 viewport상단에 고정되었을때 실행 할 함수
-  const stickyToScreenTop = () => {
-    setTopAndBottom({ top: offset, bottom: null });
-    setSticky({ isSticky: true, isAbsolute: false });
-  };
+  useEffect(() => {
+    if (!stickyHandlerRef) return;
 
-  // sticky영역이 container bottom에 도달했을때 실행 할 함수
-  const stickyToContainerBottom = () => {
-    setSticky({ isSticky: true, isAbsolute: true });
-  };
+    stickyHandlerRef.current = {
+      // sticky영역을 현재 screen(viewport)상단에 고정
+      stickToScreenTop: () => {
+        setTopAndBottom(offset, null);
+        setStickyAndAbsolute(true, false);
+        onStick?.();
+      },
 
-  // sticky영역이 container top에 도달했을때 실행 할 함수
-  const stickyToContainerTop = () => {
-    setSticky({ isSticky: false, isAbsolute: false });
-  };
+      // container bottom를 기준으로해서 sticky를 고정
+      stickToContainerBottom: () => {
+        setStickyAndAbsolute(true, true);
+        onStick?.();
+      },
 
-  /**
-   * 현재 sticky element의 상태값을 사용하여 처리하는 함수
-   */
-  const update = useCallback(() => {
-    const handlers = calculatePositionHandlers();
-    if (!handlers) return;
-
-    const { isReachContainerBottom, isReachScreenTop } = handlers;
-
-    // sticky element가 상단에 고정되었을때(sticky 상태일때)
-    if (isReachScreenTop()) {
-      // sticky상태에서 container의 bottom에 도달했을때
-      if (isReachContainerBottom()) return stickyToContainerBottom();
-      return stickyToScreenTop();
-    }
-
-    return stickyToContainerTop();
-  }, [calculatePositionHandlers]);
-
-  const handleUpdate = useCallback(() => {
-    update();
-  }, [update]);
+      unStick: () => {
+        setStickyAndAbsolute(false, false);
+        onUnStick?.();
+      },
+    };
+  }, [offset, onStick, onUnStick]);
 
   useEvent("scroll", handleUpdate, { passive: true });
   useEvent("resize", handleUpdate);
 
   return (
     <div ref={findParentFrom} className="sticky-wrap">
-      <div ref={heightRef} className="sticky-height" style={{ position: "fixed" }} />
+      <div ref={heightRef} className="sticky-height" />
       <div ref={stickyRef} className="sticky-content">
         {children}
       </div>
