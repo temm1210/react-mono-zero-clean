2ee37a1a147f235d25e2bb120912409b6ff1ea08
SongTaecWoo <temm1210@naver.com>
2021-10-19 02:38:03 +0900

    type 추가

--- packages/hooks/src/useClosetParent/index.ts
+++ packages/hooks/src/useClosetParent/index.ts
@@ -1,12 +1,19 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useRef, MutableRefObject } from "react";
 
+type ParentRef = MutableRefObject<Element | null>;
+type FindParentFrom = (node: HTMLElement | null) => void;
+
+interface Return {
+  parentRef: ParentRef;
+  findParentFrom: FindParentFrom;
+}
 /**
  * callback으로 등록된 element를 기준으로 가장 가까운 parentSelector를 찾는 hook
  */
 const useClosetParent = (parentSelector: string) => {
   const parentRef = useRef<Element | null>();
 
-  const findParentFrom = useCallback(
+  const findParentFrom: FindParentFrom = useCallback(
     (node: HTMLElement | null) => {
       const stickyParent = node?.parentElement?.closest(parentSelector);
       parentRef.current = stickyParent || null;
@@ -14,7 +21,7 @@ const useClosetParent = (parentSelector: string) => {
     [parentSelector],
   );
 
-  return { parentRef, findParentFrom };
+  return { parentRef, findParentFrom } as Return;
 };
 
 export default useClosetParent;
