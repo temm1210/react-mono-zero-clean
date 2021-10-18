229bf8835eeb8cb112e2224d0010705adf23a848
SongTaecWoo <temm1210@naver.com>
2021-10-19 03:34:09 +0900

    parentSelector 사용

--- packages/design-systems/src/Sticky/StickyContainer.tsx
+++ packages/design-systems/src/Sticky/StickyContainer.tsx
@@ -1,11 +1,12 @@
+import parentSelector from "./parentSelector";
 import "./StickyContainer.scss";
 
 export interface Props {
   children: React.ReactNode;
 }
 
-function StickyContainer({ children }: Props) {
-  return <div className="sticky-container">{children}</div>;
-}
+const StickyContainer = ({ children }: Props) => {
+  return <div className={parentSelector}>{children}</div>;
+};
 
 export default StickyContainer;
