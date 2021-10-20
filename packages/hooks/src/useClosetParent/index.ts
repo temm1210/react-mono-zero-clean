d66a34862d1a8ca9f156c122b090a3077b286087
SongTaecWoo <temm1210@naver.com>
2021-10-20 16:18:46 +0900

    ref -> state로 변경

--- packages/hooks/src/useClosetParent/index.ts
+++ packages/hooks/src/useClosetParent/index.ts
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useState } from "react";
 
 type ParentNode = Element | null;
 type FindParentFrom = (node: HTMLElement | null) => void;
@@ -11,17 +11,18 @@ interface Return {
  * callback으로 등록된 element를 기준으로 가장 가까운 parentSelector를 찾는 hook
  */
 const useClosetParent = (parentSelector: string) => {
-  const parentRef = useRef<Element | null>();
+  const [parentNode, setParentNode] = useState<Element | null>();
 
   const findParentFrom: FindParentFrom = useCallback(
     (node: HTMLElement | null) => {
       const stickyParent = node?.parentElement?.closest(parentSelector);
-      parentRef.current = stickyParent || null;
+      console.log("stickyParent:", stickyParent);
+      setParentNode(stickyParent || null);
     },
     [parentSelector],
   );
 
-  return { parentNode: parentRef.current, findParentFrom } as Return;
+  return { parentNode, findParentFrom } as Return;
 };
 
 export default useClosetParent;
