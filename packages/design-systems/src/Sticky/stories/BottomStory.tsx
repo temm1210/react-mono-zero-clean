46bd69b732d7006118ab92358e73e1c3a0661f5f
SongTaecWoo <temm1210@naver.com>
2021-10-20 18:10:33 +0900

    bottom story 추가

---
+++ packages/design-systems/src/Sticky/stories/BottomStory.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { Story } from "@storybook/react";
+import Sticky, { Props as StickyProps } from "../Sticky";
+import StickyContainer from "../StickyContainer";
+
+const Placeholder = () => <div style={{ padding: "20px 0" }}>placeholder</div>;
+
+const BaseStory: Story<StickyProps> = (args) => {
+  const { children, ...rest } = args;
+
+  return (
+    <div>
+      <StickyContainer>
+        <div>{Array(10).fill(null).map(Placeholder)}</div>
+        <Sticky {...rest}>{children}</Sticky>
+        <div>{Array(10).fill(null).map(Placeholder)}</div>
+      </StickyContainer>
+
+      <div style={{ backgroundColor: "#dcdde1" }}>{Array(7).fill(null).map(Placeholder)}</div>
+    </div>
+  );
+};
+
+export default BaseStory;
--- packages/design-systems/src/Sticky/stories/Sticky.stories.tsx
+++ packages/design-systems/src/Sticky/stories/Sticky.stories.tsx
@@ -2,6 +2,7 @@ import { Meta } from "@storybook/react";
 import { Sticky, StickyContainer } from "../index";
 import BaseStory from "./BaseStory";
 import PushStory from "./PushStory";
+import BottomStory from "./BottomStory";
 
 export default {
   component: StickyContainer,
@@ -9,10 +10,8 @@ export default {
   title: "Design Systems/Sticky",
 } as Meta;
 
-export const Base = BaseStory.bind({});
-Base.args = {
-  top: 10,
-  children: (
+const Children = () => {
+  return (
     <div style={{ backgroundColor: "#2ecc71" }}>
       <div>sticky</div>
       <div>sticky</div>
@@ -25,7 +24,20 @@ Base.args = {
       <div>sticky</div>
       <div>sticky</div>
     </div>
-  ),
+  );
+};
+
+export const Base = BaseStory.bind({});
+Base.args = {
+  top: 10,
+  children: <Children />,
+};
+
+export const Bottom = BottomStory.bind({});
+Bottom.args = {
+  mode: "bottom",
+  bottom: 20,
+  children: <Children />,
 };
 
 export const Push = PushStory.bind({});
