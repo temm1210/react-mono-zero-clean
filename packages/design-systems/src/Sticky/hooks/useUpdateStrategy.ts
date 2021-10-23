a5aa7334d382defa1d59fe0bc4aeec530fd9b0ef
SongTaecWoo <temm1210@naver.com>
2021-10-23 13:39:07 +0900

    update hook 생성

---
+++ packages/design-systems/src/Sticky/hooks/useUpdateStrategy.ts
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { useDeepCompareEffect } from "@project/react-hooks";
+import { StatusHandler, StickyMode } from "../types";
+import { PositionsReturn } from "./usePositions";
+
+export type StrategyUpdater = () => void;
+export type Strategy = Record<StickyMode, StrategyUpdater>;
+
+/**
+ * mode에 따라 실행할 update 함수를 정의
+ */
+const useStrategy = (statusHandler: StatusHandler | null, positionHandler: PositionsReturn, mode: StickyMode) => {
+  const [updater, setUpdater] = useState<Strategy | null>(null);
+
+  useDeepCompareEffect(() => {
+    const getHandlersFn = () => {
+      const handler = positionHandler();
+      if (!handler || !statusHandler) return;
+      return { ...handler, ...statusHandler };
+    };
+
+    setUpdater({
+      top() {
+        const getHandlers = getHandlersFn();
+        if (!getHandlers) return;
+
+        const { isReachScreenTop, isReachContainerBottomFrom, stickToContainerBottom, stickToScreenTop, unStick } =
+          getHandlers;
+
+        // sticky 상태일때
+        if (isReachScreenTop()) {
+          if (isReachContainerBottomFrom(mode)) {
+            return stickToContainerBottom();
+          }
+          return stickToScreenTop();
+        }
+        // sticky 상태가 아닐때
+        return unStick();
+      },
+
+      bottom() {
+        const getHandlers = getHandlersFn();
+        if (!getHandlers) return;
+
+        const {
+          isReachScreenBottom,
+          isReachContainerBottomFrom,
+          stickToContainerBottom,
+          stickyToScreenBottom,
+          unStick,
+        } = getHandlers;
+
+        // sticky 상태일때
+        if (isReachScreenBottom()) {
+          if (isReachContainerBottomFrom(mode)) {
+            return stickToContainerBottom();
+          }
+          return stickyToScreenBottom();
+        }
+        // sticky 상태가 아닐때
+        return unStick();
+      },
+    });
+  }, [positionHandler, mode]);
+
+  return updater ? updater[mode] : null;
+};
+
+export default useStrategy;
