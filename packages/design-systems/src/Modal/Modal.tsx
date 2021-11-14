912a00975a10f30231a98aef503d4e31a030c029
SongTaecWoo <temm1210@naver.com>
2021-11-14 23:12:41 +0900

    Add: Modal 기본 코드 작성

---
+++ packages/design-systems/src/Modal/Modal.tsx
@@ -0,0 +1,11 @@
+import Portal from "Portal";
+
+const Modal = () => {
+  return (
+    <Portal portalClassName="modal-portal__container">
+      <div>Modal</div>
+    </Portal>
+  );
+};
+
+export default Modal;
