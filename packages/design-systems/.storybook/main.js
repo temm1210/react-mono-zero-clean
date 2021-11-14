086ba4912449833897f9df1a5e60b9439277bae7
SongTaecWoo <temm1210@naver.com>
2021-11-14 23:57:41 +0900

    Update: storybook webpack 설정에 absolute path추가

--- packages/design-systems/.storybook/main.js
+++ packages/design-systems/.storybook/main.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const postcssNormalize = require("postcss-normalize");
+const paths = require("./paths");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
@@ -8,6 +9,9 @@ module.exports = {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
+
+    // storybook내에서 절대경로 설정
+    config.resolve.modules = [paths.appSrc, "node_modules"];
 
     // Make whatever fine-grained changes you need
     config.module.rules.push({
@@ -52,7 +56,7 @@ module.exports = {
         },
       ],
 
-      include: path.resolve(__dirname, "../"),
+      include: paths.appSrc,
     });
 
     return config;
