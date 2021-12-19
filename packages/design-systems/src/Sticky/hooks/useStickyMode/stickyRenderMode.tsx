8b62a4ab8da78e9c447f97d93efc3190d2ee6648
SongTaecWoo <temm1210@naver.com>
2021-12-20 00:22:33 +0900

    Update: stickyRenderMode의 props에 curry형태로 mode추가

--- packages/design-systems/src/Sticky/hooks/useStickyMode/stickyRenderMode.tsx
+++ packages/design-systems/src/Sticky/hooks/useStickyMode/stickyRenderMode.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { MutableRefObject, PropsWithChildren } from "react";
+import { MutableRefObject, PropsWithChildren, useMemo } from "react";
 import { FindParentFrom } from "@project/react-hooks/dist/useClosetParent";
 import { StickyMode } from "../../types";
 
@@ -18,36 +18,41 @@ interface StickyModeStyleProps {
   fakeStyle: FakeStyle;
 }
 
-export type StickyRenderModeReturn = Record<StickyMode, JSX.Element>;
+export type StickyRenderModeProps = PropsWithChildren<StickyModeStyleProps>;
+export type StickyModeComponent = (props: StickyRenderModeProps) => JSX.Element;
 
+export type StickyRenderModeReturn = (refs: StickyRenderModeRefProps) => (mode: StickyMode) => StickyModeComponent;
 // curry 함수컴포넌트
-const stickyRenderMode =
-  ({ fakeRef, stickyRef, findParentFrom }: StickyRenderModeRefProps) =>
-  ({
-    calculateStickyStyle,
-    stickyClassNames,
-    fakeStyle,
-    children,
-  }: PropsWithChildren<StickyModeStyleProps>): StickyRenderModeReturn => ({
-    top: (
-      <div ref={findParentFrom} className="sticky-wrap">
-        {/* fake element */}
-        <div ref={fakeRef} className="sticky__fake" style={fakeStyle} />
-        <div ref={stickyRef} className={stickyClassNames} style={calculateStickyStyle()}>
-          {children}
-        </div>
-      </div>
-    ),
+const stickyRenderMode: StickyRenderModeReturn =
+  ({ fakeRef, stickyRef, findParentFrom }) =>
+  (mode) =>
+  ({ calculateStickyStyle, stickyClassNames, fakeStyle, children }) => {
+    const modeMapper = useMemo(
+      () => ({
+        top: (
+          <div ref={findParentFrom} className="sticky-wrap">
+            {/* fake element */}
+            <div ref={fakeRef} className="sticky__fake" style={fakeStyle} />
+            <div ref={stickyRef} className={stickyClassNames} style={calculateStickyStyle()}>
+              {children}
+            </div>
+          </div>
+        ),
 
-    bottom: (
-      <div ref={findParentFrom} className="sticky-wrap">
-        <div ref={stickyRef} className={stickyClassNames} style={calculateStickyStyle()}>
-          {children}
-        </div>
-        {/* fake element */}
-        <div ref={fakeRef} className="sticky__fake" style={fakeStyle} />
-      </div>
-    ),
-  });
+        bottom: (
+          <div ref={findParentFrom} className="sticky-wrap">
+            <div ref={stickyRef} className={stickyClassNames} style={calculateStickyStyle()}>
+              {children}
+            </div>
+            {/* fake element */}
+            <div ref={fakeRef} className="sticky__fake" style={fakeStyle} />
+          </div>
+        ),
+      }),
+      [calculateStickyStyle, children, fakeStyle, stickyClassNames],
+    );
+
+    return modeMapper[mode];
+  };
 
 export default stickyRenderMode;
