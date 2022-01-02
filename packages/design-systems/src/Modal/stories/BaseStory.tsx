4a804c59402ad3909c4db16104dfa68824940716
SongTaecWoo <temm1210@naver.com>
2022-01-02 14:13:56 +0900

    Update: Portal에 className이 존재할때 로직 추가

--- packages/design-systems/src/Modal/stories/BaseStory.tsx
+++ packages/design-systems/src/Modal/stories/BaseStory.tsx
@@ -6,13 +6,22 @@ import Modal, { Props as ModalProps } from "../Modal";
 const BaseStory: Story<ModalProps> = (args) => {
   const { children } = args;
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
 
   const openModal = () => {
     setIsOpen(true);
   };
 
+  const openModal2 = () => {
+    setIsOpen2(true);
+  };
+
   const closeModal = () => {
     setIsOpen(false);
+  };
+
+  const closeModal2 = () => {
+    setIsOpen2(false);
   };
 
   console.log(isOpen);
@@ -25,7 +34,16 @@ const BaseStory: Story<ModalProps> = (args) => {
       <button type="button" onClick={closeModal}>
         close modal
       </button>
+
+      <button type="button" onClick={openModal2}>
+        open modal2
+      </button>
+
+      <button type="button" onClick={closeModal2}>
+        close modal2
+      </button>
       <Modal isOpen={isOpen}>{children}</Modal>
+      <Modal isOpen={isOpen2}>{children}</Modal>
     </div>
   );
 };
---
+++ packages/design-systems/src/Portal/Portal.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import usePortalElement from "./hooks/usePortalElement";
+
+export interface Props {
+  /** portal할 부모 element의 class name */
+  className?: string;
+  children: React.ReactNode;
+}
+
+const Portal = ({ className = "portal-container", children }: Props) => {
+  const { portalElement, isExistParent } = usePortalElement(className);
+
+  useEffect(() => {
+    if (isExistParent || !portalElement) return;
+
+    document.body.appendChild(portalElement);
+    return () => {
+      document.body.removeChild(portalElement);
+    };
+  }, [isExistParent, portalElement]);
+
+  return portalElement && createPortal(children, portalElement);
+};
+
+export default Portal;
