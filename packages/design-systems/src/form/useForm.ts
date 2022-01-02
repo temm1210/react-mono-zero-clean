b685ff8e7c4bd7b57d6e643c7fe7ff080f78a565
Carpest <temm1210@naver.com>
2022-01-03 03:05:18 +0900

    Update: formAdapter 작성 (#36)
    * Install: lodash install
    
    * Update: formAdapter 작성

--- packages/design-systems/src/form/useForm.ts
+++ packages/design-systems/src/form/useForm.ts
@@ -1,36 +1,38 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import mapValues from "lodash/mapValues";
+
 type HTMLTextInputElement = HTMLInputElement | HTMLTextAreaElement;
 
 export interface Field {
   ref: HTMLTextInputElement;
 }
 
-export type Fields<K> = {
-  [key in keyof K]: Field;
+export type Fields<F> = {
+  [key in keyof F]: Field;
 };
-export type OnSubmit<K> = (data: FormData<K>) => void;
+
+type FormData<F> = {
+  [key in keyof F]: string;
+};
+export type OnSubmit<F> = (data: FormData<F>) => void;
 
 export interface RegisterReturnRef {
   name: string;
   ref: (ref: HTMLTextInputElement | null) => void;
 }
 export type Register = (name: string) => RegisterReturnRef;
-export type HandleSubmit<K> = (onSubmit: OnSubmit<K>) => (e: React.MouseEvent<HTMLElement>) => void;
+export type HandleSubmit<F> = (onSubmit: OnSubmit<F>) => (e: React.MouseEvent<HTMLElement>) => void;
 
 // K는 useForm<T> 에서 T에 해당됨
-export interface UseFormReturn<K = Record<string, any>> {
+export interface UseFormReturn<F = Record<string, any>> {
   register: Register;
-  handleSubmit: HandleSubmit<K>;
+  handleSubmit: HandleSubmit<F>;
 }
-
-type FormData<K> = {
-  [key in keyof K]: string;
-};
 
 // S는 useForm<T> 에서 T에 해당됨
 // N는 form들의 name이됨
-const useForm = <S, N extends keyof S = keyof S>(): UseFormReturn<S> => {
-  const fields = {} as Fields<S>;
+const useForm = <F, N extends keyof F = keyof F>(): UseFormReturn<F> => {
+  const fields = {} as Fields<F>;
 
   // form tag에 필요한 attribute 설정
   const register: Register = (name) => {
@@ -49,12 +51,18 @@ const useForm = <S, N extends keyof S = keyof S>(): UseFormReturn<S> => {
     };
   };
 
-  const handleSubmit: HandleSubmit<S> = (onSubmit) => (e) => {
+  // onSubmit에 전달될 데이터의 형태를 정의
+  const formAdapter = <D extends Field>(data: D) => {
+    return data.ref.value;
+  };
+
+  const handleSubmit: HandleSubmit<F> = (onSubmit) => (e) => {
     e.preventDefault();
 
-    console.log("fields:", fields);
+    const adaptedFormData = mapValues(fields, formAdapter);
+    console.log("adaptedFormData:", adaptedFormData);
     // 변형된 데이터를 실제 form에 전달
-    onSubmit(fields as any);
+    onSubmit(adaptedFormData);
   };
 
   return { register, handleSubmit };
