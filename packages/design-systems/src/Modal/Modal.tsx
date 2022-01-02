a0905f0e9b4f7d3beb34d79e25b6043bed110801
Carpest <temm1210@naver.com>
2022-01-03 00:53:55 +0900

    Update: Modal dimmer 클릭시 Modal close (#33)
    * Update: jsx-ally turn off
    
    * Update: Modal dimmer클릭시 Modal close

--- packages/design-systems/src/Modal/Modal.tsx
+++ packages/design-systems/src/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, MouseEvent, useRef } from "react";
 import Portal from "Portal";
 import cx from "clsx";
 import { useEvent } from "@project/react-hooks";
@@ -8,17 +8,22 @@ export interface Props {
   children: React.ReactNode;
   /** Modal의 open여부 */
   isOpen: boolean;
+  /** Modal을 close하는 함수 */
+  onClose: () => void;
+  /** Modal의 portal class name */
+  portalName?: string;
 }
 
-const Modal = ({ isOpen, children }: Props) => {
+const Modal = ({ isOpen, children, onClose, portalName = "modal-portal" }: Props) => {
   const [isEndAnimation, setIsEndAnimation] = useState(false);
+  const contentRef = useRef<HTMLDivElement | null>(null);
 
   const onTransitionEnd = useCallback(() => {
     setIsEndAnimation(isOpen);
   }, [isOpen]);
 
   useEffect(() => {
-    if (isOpen) setTimeout(onTransitionEnd, 50);
+    if (isOpen) setTimeout(onTransitionEnd, 30);
   }, [isOpen, onTransitionEnd]);
 
   useEffect(() => {
@@ -35,13 +40,21 @@ const Modal = ({ isOpen, children }: Props) => {
     isEndAnimation && isOpen ? "modal-content__body--open" : "modal-content__body--close",
   );
 
+  const onClick = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target === contentRef.current) {
+      onClose();
+    }
+  };
+
   return (
     <>
       {(isOpen || isEndAnimation) && (
-        <Portal className="modal-portal">
+        <Portal className={portalName}>
           <div className="modal-container">
-            <div className="modal-content">
-              <div className={contentClassNames}>{children}</div>
+            <div className="modal-content" onClick={onClick}>
+              <div ref={contentRef} className={contentClassNames}>
+                {children}
+              </div>
             </div>
           </div>
         </Portal>
