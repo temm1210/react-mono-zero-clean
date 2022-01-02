939d1e5030feef0a7ad4fbc4822f60b54bf15cd9
SongTaecWoo <temm1210@naver.com>
2022-01-02 19:31:15 +0900

    Update: Modal animation 설정

--- packages/design-systems/src/Modal/Modal.tsx
+++ packages/design-systems/src/Modal/Modal.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState, useCallback } from "react";
 import Portal from "Portal";
+import cx from "clsx";
+import { useEvent } from "@project/react-hooks";
+import "./Modal.scss";
 
 export interface Props {
   children: React.ReactNode;
@@ -7,8 +11,43 @@ export interface Props {
 }
 
 const Modal = ({ isOpen, children }: Props) => {
-  console.log("isOpen:", isOpen);
-  return <>{isOpen && <Portal className="modal-portal__container">testset{children}</Portal>}</>;
+  const [isEndAnimation, setIsEndAnimation] = useState(false);
+
+  const onTransitionEnd = useCallback(() => {
+    setIsEndAnimation(isOpen);
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (isOpen) setTimeout(onTransitionEnd, 50);
+  }, [isOpen, onTransitionEnd]);
+
+  useEffect(() => {
+    if (isOpen) document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen]);
+
+  useEvent("transitionend", onTransitionEnd);
+
+  const contentClassNames = cx(
+    "modal-content__body",
+    isEndAnimation && isOpen ? "modal-content__body--open" : "modal-content__body--close",
+  );
+
+  return (
+    <>
+      {(isOpen || isEndAnimation) && (
+        <Portal className="modal-portal">
+          <div className="modal-container">
+            <div className="modal-content">
+              <div className={contentClassNames}>{children}</div>
+            </div>
+          </div>
+        </Portal>
+      )}
+    </>
+  );
 };
 
 export default Modal;
