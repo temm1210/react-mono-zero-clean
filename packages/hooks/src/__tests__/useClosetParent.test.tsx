af146ce97e062a6e814776a148f5da5fe5703412
temm1210 <temm1210@naver.com>
2022-06-19 22:54:03 +0900

    feat: test case 추가

--- packages/hooks/src/__tests__/useClosetParent.test.tsx
+++ packages/hooks/src/__tests__/useClosetParent.test.tsx
@@ -4,9 +4,9 @@ import { renderHook } from "@testing-library/react-hooks";
 import useClosetParent from "../useClosetParent";
 
 describe("useClosetParent", () => {
-  it("useClosetParent는 주어진 className에 해당하는 parent element를 찾을 수 있다", () => {
-    const PARENT_CLASS_NAME = "parent-test";
+  const PARENT_CLASS_NAME = "parent-test";
 
+  it("useClosetParent는 주어진 className에 해당하는 parent element를 찾을 수 있다", () => {
     const { result } = renderHook(() => useClosetParent(`.${PARENT_CLASS_NAME}`));
 
     render(
@@ -15,8 +15,18 @@ describe("useClosetParent", () => {
       </div>,
     );
 
-    console.log("className:", result.current.parentNode?.className);
-
     expect(result.current.parentNode?.className).toEqual(PARENT_CLASS_NAME);
+  });
+
+  it("useClosetParent는 주어진 className에 해당하는 parent element이 없다면 null을 return 해야한다", () => {
+    const { result } = renderHook(() => useClosetParent(`.${PARENT_CLASS_NAME}`));
+
+    render(
+      <div>
+        <div ref={result.current.findParentFrom} />
+      </div>,
+    );
+
+    expect(result.current.parentNode).toBeNull();
   });
 });
