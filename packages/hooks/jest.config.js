71c799ba0b74071ba496371dcdbc2850a1cd9d6a
temm1210 <temm1210@naver.com>
2022-06-23 16:53:13 +0900

    feat: dist/* test제외

--- packages/hooks/jest.config.js
+++ packages/hooks/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   collectCoverageFrom: ["src/**/{!(*.d.ts),}.{ts,js,tsx,jsx}"],
-  coveragePathIgnorePatterns: ["/node_modules/", "package.json", "@types/"],
+  coveragePathIgnorePatterns: ["/node_modules/", "package.json", "dist/", "@types/"],
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
