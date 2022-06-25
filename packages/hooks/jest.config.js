4d6bf46e13e131c6bc633e693a6e5164767bd028
temm1210 <temm1210@naver.com>
2022-06-25 14:43:36 +0900

    refactor: jest config js 변경

--- packages/hooks/jest.config.js
+++ packages/hooks/jest.config.js
@@ -1,12 +1,16 @@
 module.exports = {
+  preset: "ts-jest",
   verbose: true,
   clearMocks: true,
-  collectCoverageFrom: ["src/**/{!(*.d.ts),}.{ts,js,tsx,jsx}"],
+  testMatch: ["<rootDir>/src/**/*.test.(ts|tsx)"],
+  // collectCoverageFrom: ["src/**/{!(*.d.ts),}.{ts,js,tsx,jsx}"],
   coveragePathIgnorePatterns: ["/node_modules/", "package.json", "dist/", "@types/"],
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
   },
 };
