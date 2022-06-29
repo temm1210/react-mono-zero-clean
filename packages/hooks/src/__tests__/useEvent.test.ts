f12ca22f7ca6023a9a4faa0dc47c6708654cb32b
temm1210 <temm1210@naver.com>
2022-06-29 21:38:19 +0900

    feat: test case 추가

--- packages/hooks/src/__tests__/useEvent.test.ts
+++ packages/hooks/src/__tests__/useEvent.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { renderHook } from "@testing-library/react";
 import useEvent, {
   UseEventListenerOptions,
@@ -16,12 +17,14 @@ interface Props {
 const props: Props[] = [
   {
     eventType: "click",
-    listener: jest.fn(),
+    listener: () => {},
+    options: { capture: true },
     context: window,
   },
   {
     eventType: "change",
-    listener: jest.fn(),
+    listener: () => {},
+    options: { capture: false },
     context: document.createElement("div"),
   },
 ];
@@ -30,6 +33,9 @@ describe("useEvent", () => {
   beforeEach(() => {
     jest.spyOn(window, "addEventListener");
     jest.spyOn(window, "removeEventListener");
+
+    jest.spyOn(props[1].context, "addEventListener");
+    jest.spyOn(props[1].context, "removeEventListener");
   });
   it("mount/unmount시 window.addEventListener/window.removeEventListener가 eventType, listener, option를 인자로 가지고 호출된다.", () => {
     const { unmount } = renderHook(() =>
@@ -54,9 +60,6 @@ describe("useEvent", () => {
   });
 
   it("mount/unmount시 context.addEventListener/context.removeEventListener가 eventType, listener, option를 인자로 가지고 호출된다.", () => {
-    jest.spyOn(props[1].context, "addEventListener");
-    jest.spyOn(props[1].context, "removeEventListener");
-
     const { unmount } = renderHook(() =>
       useEvent(props[1].eventType, props[1].listener, props[1].options, props[1].context),
     );
@@ -82,6 +85,102 @@ describe("useEvent", () => {
     expect(props[1].context.removeEventListener).toHaveBeenCalledTimes(1);
 
     expect(props[1].context.removeEventListener).toHaveBeenLastCalledWith(
+      props[1].eventType,
+      props[1].listener,
+      props[1].options,
+    );
+  });
+  it("dependency list가 변경될 시addEventListener/removeEventListener는 다시 호출된다.", () => {
+    const { rerender } = renderHook(
+      ({ eventType, listener, options, context }: Props) => useEvent(eventType, listener, options, context),
+      {
+        initialProps: {
+          eventType: props[0].eventType,
+          listener: props[0].listener,
+          options: props[0].options,
+          context: props[0].context,
+        },
+      },
+    );
+
+    expect(window.addEventListener).toHaveBeenCalledTimes(1);
+    expect(window.addEventListener).toHaveBeenLastCalledWith(props[0].eventType, props[0].listener, props[0].options);
+
+    expect(window.removeEventListener).toHaveBeenCalledTimes(0);
+
+    // eventType이 변경될때
+    rerender({
+      eventType: props[1].eventType,
+      listener: props[0].listener,
+      options: props[0].options,
+      context: props[0].context,
+    });
+
+    expect(window.addEventListener).toHaveBeenCalledTimes(2);
+    expect(window.addEventListener).toHaveBeenLastCalledWith(props[1].eventType, props[0].listener, props[0].options);
+
+    expect(window.removeEventListener).toHaveBeenCalledTimes(1);
+    expect(window.removeEventListener).toHaveBeenLastCalledWith(
+      props[0].eventType,
+      props[0].listener,
+      props[0].options,
+    );
+
+    // listener가 변경될때
+    rerender({
+      eventType: props[1].eventType,
+      listener: props[1].listener,
+      options: props[0].options,
+      context: props[0].context,
+    });
+
+    expect(window.addEventListener).toHaveBeenCalledTimes(3);
+    expect(window.addEventListener).toHaveBeenLastCalledWith(props[1].eventType, props[1].listener, props[0].options);
+
+    expect(window.removeEventListener).toHaveBeenCalledTimes(2);
+    expect(window.removeEventListener).toHaveBeenLastCalledWith(
+      props[1].eventType,
+      props[0].listener,
+      props[0].options,
+    );
+
+    // options가 변경될때
+    rerender({
+      eventType: props[1].eventType,
+      listener: props[1].listener,
+      options: props[1].options,
+      context: props[0].context,
+    });
+
+    expect(window.addEventListener).toHaveBeenCalledTimes(4);
+    expect(window.addEventListener).toHaveBeenLastCalledWith(props[1].eventType, props[1].listener, props[1].options);
+
+    expect(window.removeEventListener).toHaveBeenCalledTimes(3);
+    expect(window.removeEventListener).toHaveBeenLastCalledWith(
+      props[1].eventType,
+      props[1].listener,
+      props[0].options,
+    );
+
+    // context가 변경될때
+    rerender({
+      eventType: props[1].eventType,
+      listener: props[1].listener,
+      options: props[1].options,
+      context: props[1].context,
+    });
+
+    expect(window.addEventListener).toHaveBeenCalledTimes(4);
+
+    expect(window.removeEventListener).toHaveBeenCalledTimes(4);
+    expect(window.removeEventListener).toHaveBeenLastCalledWith(
+      props[1].eventType,
+      props[1].listener,
+      props[1].options,
+    );
+
+    expect(props[1].context.addEventListener).toHaveBeenCalledTimes(1);
+    expect(props[1].context.addEventListener).toHaveBeenLastCalledWith(
       props[1].eventType,
       props[1].listener,
       props[1].options,
