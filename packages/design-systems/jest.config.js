28d8f09ba292e07da1219873d9255ecf5c92c16b
temm1210 <temm1210@naver.com>
2022-08-18 00:10:59 +0900

    feat: jest config 변경

--- packages/design-systems/jest.config.js
+++ packages/design-systems/jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  collectCoverageFrom: ["src/**/{!(*.d.ts),}.{ts,js,tsx,jsx}"],
-  coveragePathIgnorePatterns: ["/node_modules/", "package.json", "@types/"],
+  preset: "ts-jest",
+  verbose: true,
+  clearMocks: true,
+  testMatch: ["<rootDir>/src/**/*.test.(ts|tsx)"],
+  // collectCoverageFrom: ["src/**/{!(*.d.ts),}.{ts,js,tsx,jsx}"],
+  coveragePathIgnorePatterns: ["/node_modules/", "package.json", "dist/", "@types/"],
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  moduleNameMapper: {
-    "^.+\\.(css|less|scss)$": "babel-jest",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
   },
 };
