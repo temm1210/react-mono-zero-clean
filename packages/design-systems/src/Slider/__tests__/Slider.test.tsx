e792f8d28523a0d9a9a99b0ced6cf8feea809d50
temm1210 <temm1210@naver.com>
2022-08-20 02:40:16 +0900

    feat: test case 추가

--- packages/design-systems/src/Slider/__tests__/Slider.test.tsx
+++ packages/design-systems/src/Slider/__tests__/Slider.test.tsx
@@ -1,8 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import Slider from "../Slider";
 
-test("role이 slider인 tag가 존재한다.", () => {
-  render(<Slider />);
+describe("Slider component", () => {
+  it("role이 slider인 tag가 존재한다.", () => {
+    render(<Slider />);
 
-  expect(screen.getByRole("slider")).toBeInTheDocument();
+    expect(screen.getByRole("slider")).toBeInTheDocument();
+  });
+
+  it("aria값이 주어진 property에 맞게 설정되어야 한다.", () => {
+    render(<Slider min={0} max={100} defaultValue={50} />);
+
+    const slider = screen.getByRole("slider");
+    expect(slider.getAttribute("aria-valuenow")).toBe(50);
+    expect(slider.getAttribute("aria-valuemax")).toBe(100);
+    expect(slider.getAttribute("aria-valuemin")).toBe(0);
+  });
 });
