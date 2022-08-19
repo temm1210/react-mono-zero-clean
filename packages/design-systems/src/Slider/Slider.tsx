de88b1cc70f47bc405738b29d9e283fb4ed486b9
temm1210 <temm1210@naver.com>
2022-08-20 02:44:56 +0900

    feat: Slider 컴포넌트 test case추가 (#61)
    * feat: aria속성 추가
    
    * feat: tabIndex, aria-orientation추가
    
    * feat: aria-label 변경
    
    * feat: test case 추가
    
    * feat: props 추가

--- packages/design-systems/src/Slider/Slider.tsx
+++ packages/design-systems/src/Slider/Slider.tsx
@@ -1,9 +1,21 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable jsx-a11y/role-has-required-aria-props */
-function Slider() {
+export interface SliderProps {
+  min?: number;
+  max?: number;
+  defaultValue?: number;
+}
+
+function Slider({ min = 0, max = 100, defaultValue = 0 }: SliderProps) {
   return (
     <div className="slider">
-      <div role="slider" />
+      <div
+        role="slider"
+        aria-label="horizontal slider"
+        aria-valuenow={defaultValue}
+        aria-valuemax={max}
+        aria-valuemin={min}
+        aria-orientation="horizontal"
+        tabIndex={0}
+      />
     </div>
   );
 }
--- packages/design-systems/src/Slider/__tests__/Slider.test.tsx
+++ packages/design-systems/src/Slider/__tests__/Slider.test.tsx
@@ -1,8 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import Slider from "../Slider";
 
-test("role이 slider인 tag가 존재한다.", () => {
-  render(<Slider />);
+describe("Slider component test", () => {
+  it("role이 slider인 tag가 존재한다.", () => {
+    render(<Slider />);
 
-  expect(screen.getByRole("slider")).toBeInTheDocument();
+    expect(screen.getByRole("slider")).toBeInTheDocument();
+  });
+
+  it("aria값이 주어진 property에 맞게 설정되어야 한다.", () => {
+    const min = 10;
+    const max = 200;
+    const defaultValue = 30;
+
+    render(<Slider min={min} max={max} defaultValue={defaultValue} />);
+
+    const slider = screen.getByRole("slider");
+    expect(slider.getAttribute("aria-valuenow")).toBe(`${defaultValue}`);
+    expect(slider.getAttribute("aria-valuemax")).toBe(`${max}`);
+    expect(slider.getAttribute("aria-valuemin")).toBe(`${min}`);
+  });
 });
