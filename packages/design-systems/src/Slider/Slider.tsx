76a8a81dc490011cabed1177845c99c14705c761
temm1210 <temm1210@naver.com>
2022-08-20 01:22:32 +0900

    feat: role attribute 추가 (#60)

--- packages/design-systems/src/Slider/Slider.tsx
+++ packages/design-systems/src/Slider/Slider.tsx
@@ -1,5 +1,11 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/role-has-required-aria-props */
 function Slider() {
-  return <div>Slider</div>;
+  return (
+    <div className="slider">
+      <div role="slider" />
+    </div>
+  );
 }
 
 export default Slider;
