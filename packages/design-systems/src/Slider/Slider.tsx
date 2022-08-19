a5d1286eeb69dce344a49075a09f8f57c3e4fd08
temm1210 <temm1210@naver.com>
2022-08-20 01:35:30 +0900

    feat: tabIndex, aria-orientation추가

--- packages/design-systems/src/Slider/Slider.tsx
+++ packages/design-systems/src/Slider/Slider.tsx
@@ -1,7 +1,15 @@
 function Slider() {
   return (
     <div className="slider">
-      <div role="slider" aria-label="slider" aria-valuenow={20} aria-valuemax={100} aria-valuemin={0} />
+      <div
+        role="slider"
+        aria-label="slider"
+        aria-valuenow={20}
+        aria-valuemax={100}
+        aria-valuemin={0}
+        aria-orientation="horizontal"
+        tabIndex={0}
+      />
     </div>
   );
 }
