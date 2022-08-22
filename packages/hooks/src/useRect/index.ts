d7809f266421ffd6756a1bb80588ebd4cd04a372
temm1210 <temm1210@naver.com>
2022-08-22 23:54:24 +0900

    feat: useRect rollback

--- packages/hooks/src/useRect/index.ts
+++ packages/hooks/src/useRect/index.ts
@@ -1,14 +1,23 @@
 import { useState } from "react";
 
-export type UseClientRect = Pick<DOMRectReadOnly, "top" | "left" | "right" | "bottom" | "height" | "width"> | null;
+export type UseClientRect = Pick<DOMRectReadOnly, "top" | "left" | "right" | "bottom" | "height" | "width">;
 
 export type UseRectRef<E extends Element = Element> = (element: E | null) => void;
 export type UseRectResult<E extends Element = Element> = [UseRectRef<E>, () => UseClientRect];
 
+const defaultState: UseClientRect = {
+  width: 0,
+  height: 0,
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+};
+
 const useRect = <F extends Element>(): UseRectResult<F> => {
   const [element, ref] = useState<Element | null>(null);
 
-  const getRect = () => element?.getBoundingClientRect() || null;
+  const getRect = () => element?.getBoundingClientRect() || defaultState;
 
   return [ref, getRect];
 };
