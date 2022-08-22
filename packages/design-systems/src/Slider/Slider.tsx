d25e6fc7ab5e1c280c1b865c5a80a7ea67d6c896
temm1210 <temm1210@naver.com>
2022-08-22 21:59:51 +0900

    feat: Slider init style  (#63)
    * feat: rail, track html 추가
    
    * feat: style 적용

--- packages/design-systems/src/Slider/Slider.tsx
+++ packages/design-systems/src/Slider/Slider.tsx
@@ -1,3 +1,5 @@
+import "./Slider.scss";
+
 export interface SliderProps {
   /** 도달할 수 있는 최소값 */
   min?: number;
@@ -10,9 +12,12 @@ export interface SliderProps {
 function Slider({ min = 0, max = 100, defaultValue = 0 }: SliderProps) {
   return (
     <div className="slider">
+      <div className="slider__rail" />
+      <div className="slider__track" />
       <div
+        className="slider__controller"
         role="slider"
-        aria-label="horizontal slider"
+        aria-label="가로방향 슬라이더"
         aria-valuenow={defaultValue}
         aria-valuemax={max}
         aria-valuemin={min}
