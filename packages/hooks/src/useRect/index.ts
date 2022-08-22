9f37807be394b4add28c9481c5cd04080097ca87
temm1210 <temm1210@naver.com>
2022-08-22 22:57:07 +0900

    fix: resolve build error (#64)

--- packages/hooks/src/useRect/index.ts
+++ packages/hooks/src/useRect/index.ts
@@ -1,23 +1,14 @@
 import { useState } from "react";
 
-export type UseClientRect = Pick<DOMRectReadOnly, "top" | "left" | "right" | "bottom" | "height" | "width">;
+export type UseClientRect = Pick<DOMRectReadOnly, "top" | "left" | "right" | "bottom" | "height" | "width"> | null;
 
 export type UseRectRef<E extends Element = Element> = (element: E | null) => void;
 export type UseRectResult<E extends Element = Element> = [UseRectRef<E>, () => UseClientRect];
 
-const defaultState: UseClientRect = {
-  width: 0,
-  height: 0,
-  top: 0,
-  left: 0,
-  bottom: 0,
-  right: 0,
-};
-
 const useRect = <F extends Element>(): UseRectResult<F> => {
   const [element, ref] = useState<Element | null>(null);
 
-  const getRect = () => element?.getBoundingClientRect() || defaultState;
+  const getRect = () => element?.getBoundingClientRect() || null;
 
   return [ref, getRect];
 };
