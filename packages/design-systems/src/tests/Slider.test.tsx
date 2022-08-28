d7daa2219c1ef322731a853b1d42708fcbeb112a
temm1210 <temm1210@naver.com>
2022-08-28 19:24:32 +0900

    refactor: test case 수정

--- packages/design-systems/src/tests/Slider.test.tsx
+++ packages/design-systems/src/tests/Slider.test.tsx
@@ -21,33 +21,76 @@ describe("Slider component test", () => {
     expect(slider.getAttribute("aria-valuemin")).toBe(`${min}`);
   });
 
-  it("drag가 끝나는 지점에서의 값(style, value)이 올바르게 계산되어야한다.", () => {
-    const { container, getByRole } = render(<Slider min={10} max={200} defaultValue={10} />);
+  it("controller가 있는 지점에서의 값(style, value)이 올바르게 계산되어야한다.", () => {
+    const max = 200;
+    const min = 10;
+
+    const { container, getByRole } = render(<Slider min={min} max={max} />);
 
     const sliderContainer = container.getElementsByClassName("slider")[0];
 
+    const left = 40;
+    const width = 300;
+
     sliderContainer.getBoundingClientRect = jest.fn(() => {
       return {
-        width: 300,
+        width,
         height: 10,
         top: 0,
-        left: 20,
+        left,
         bottom: 0,
         right: 0,
       } as DOMRect;
     });
 
-    const sliderController = getByRole("slider");
-
-    fireEvent.mouseDown(sliderController);
-    fireEvent.mouseMove(sliderController, { clientX: 100 });
-    fireEvent.mouseUp(sliderController);
-
-    expect(sliderController).toHaveAttribute("aria-valuenow", `${60}`);
-    expect(sliderController).toHaveStyle(`left:26%`);
-
+    const controller = getByRole("slider");
     const sliderTack = container.getElementsByClassName("slider__track")[0];
-    expect(sliderTack).toHaveStyle(`width:26%`);
+
+    console.log("sliderContainer:", sliderContainer);
+
+    // minxClientX <= clientX <= maxClientX
+    const maxClientX = width + left;
+    const minxClientX = left;
+
+    // 초기 값
+    expect(controller).toHaveAttribute("aria-valuenow", `${min}`);
+    expect(controller).toHaveStyle(`left:0%`);
+    expect(sliderTack).toHaveStyle(`width:0%`);
+
+    // controller를 rail의 특정 부분에 click시
+    fireEvent.mouseDown(controller, { clientX: 200 });
+    fireEvent.mouseUp(controller);
+
+    expect(controller).toHaveAttribute("aria-valuenow", `${111}`);
+    expect(controller).toHaveStyle(`left:53%`);
+    expect(sliderTack).toHaveStyle(`width:53%`);
+
+    // controller를 start지점으로 drag시
+    fireEvent.mouseDown(controller, { clientX: 150 });
+    fireEvent.mouseMove(controller, { clientX: minxClientX });
+    fireEvent.mouseUp(controller);
+
+    expect(controller).toHaveAttribute("aria-valuenow", `${min}`);
+    expect(controller).toHaveStyle(`left:0%`);
+    expect(sliderTack).toHaveStyle(`width:0%`);
+
+    // controller를 rail의 특정(min < rail < max) 부분에 drag시
+    fireEvent.mouseDown(controller, { clientX: 0 });
+    fireEvent.mouseMove(controller, { clientX: 150 });
+    fireEvent.mouseUp(controller);
+
+    expect(controller).toHaveAttribute("aria-valuenow", `${79}`);
+    expect(controller).toHaveStyle(`left:36%`);
+    expect(sliderTack).toHaveStyle(`width:36%`);
+
+    // controller를 끝까지 drag시
+    fireEvent.mouseDown(controller, { clientX: 0 });
+    fireEvent.mouseMove(controller, { clientX: maxClientX });
+    fireEvent.mouseUp(controller);
+
+    expect(controller).toHaveAttribute("aria-valuenow", `${max}`);
+    expect(controller).toHaveStyle(`left:100%`);
+    expect(sliderTack).toHaveStyle(`width:100%`);
   });
 
   it("max <= min 일시 error를 발생시킨다.", () => {
